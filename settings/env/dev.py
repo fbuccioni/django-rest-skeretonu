1f023b2366ac7092642a93e087afdd358bea0c6e
Felipe A Buccioni <falcacibar@gmail.com>
2018-03-05 13:11:14 -0300

    Init commit

---
+++ settings/env/dev.py
@@ -0,0 +1,19 @@
+from ..main import *
+
+# Development configuration
+
+# If you use development hosts separately
+#ALLOWED_HOSTS += ['app-dev.domain.tld']
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': '',
+        # 'USER': '',
+        # 'PASS': '',
+        # 'HOST': '',
+    }
+}
---
+++ settings/env/prod.py
@@ -0,0 +1,17 @@
+from ..main import *
+
+# Production configuration
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': '',
+        # 'USER': '',
+        # 'PASS': '',
+        # 'HOST': '',
+    }
+}
