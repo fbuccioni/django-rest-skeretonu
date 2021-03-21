d8d352498ff9d89e4d5d7b62853e7906fedd67b5
Felipe Buccioni <falcacibar@gmail.com>
2021-03-21 04:02:38 -0300

    Fix: Remove database config examples from env confs

--- settings/env/dev.py
+++ settings/env/dev.py
@@ -4,16 +4,3 @@ from ..main import *
 
 # If you use development hosts separately
 #ALLOWED_HOSTS += ['app-dev.domain.tld']
-
-# Database - # AVOID THIS! Try dotenv URL schema
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': '',
-#         'USER': '',
-#         'PASS': '',
-#         'HOST': '',
-#     }
-# }
--- settings/env/local.py
+++ settings/env/local.py
@@ -5,12 +5,3 @@ from .dev import *
 # If you use development hosts separately
 #ALLOWED_HOSTS += ['local-host.app.local']
 
-# Database - # AVOID THIS! Try dotenv URL schema
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
