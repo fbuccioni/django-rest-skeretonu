3587d4b334e42f80fa39916bc62a42db709aacb9
Felipe Buccioni <falcacibar@gmail.com>
2021-05-21 13:51:32 -0400

    Fix: Admin urls from contrib instead `apps.admin`

--- apps/urls.py
+++ apps/urls.py
@@ -2,15 +2,15 @@
 URL Configuration for proeject
 """
 
-from django.contrib import admin
 from django.urls import path, include
 from django.views.i18n import JavaScriptCatalog
 
+import apps.admin
 import apps.skeretonu.urls
 
 urlpatterns = [
     path('_/jet/', include('jet.urls', 'jet')),
     path('js/i18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
-    path('admin/', admin.site.urls),
+    path('admin/', apps.admin.site.urls),
     path('', include(apps.skeretonu.urls))
 ]
