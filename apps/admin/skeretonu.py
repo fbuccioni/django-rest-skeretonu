a64357bcda3475119a094c1b8f058591a002c446
Felipe Buccioni <falcacibar@gmail.com>
2021-06-22 22:37:53 -0400

    Improve: Admin fixes and comments

--- apps/admin/skeretonu.py
+++ apps/admin/skeretonu.py
@@ -1,5 +1,9 @@
 """
-django-skeretonu.rst: App admin example, this file must have the name of the app
+App admin example, this file must have the name of the app
+
+Don't add classes manually the classes automatically are added when have
+the same name of the model, if you don't want to add it automatically
+added use `ignore_auto` property as `True`
 """
 from django.contrib import admin
 from django.utils.safestring import mark_safe
